c2eaf24b2331fa053385439bd03cfdfa4088e680
tusharkandpal <kandpal.tushar@gmail.com>
2021-01-03 16:38:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import "./App.css";
+import Show from "./components/Show";
+
+const SearchAPI = "http://api.tvmaze.com/search/shows?q=";
+const FeaturedAPI = "http://api.tvmaze.com/shows?page=1";
 
 function App() {
+const [shows, setShows] = useState([]);
+const [searchTerm, setSearchTerm] = useState('');
+
+useEffect(() => {
+  fetch(FeaturedAPI)
+    .then(res => res.json())
+    .then(data => {
+      const featured = data.filter((show) => show.id <= 350 )
+      setShows(featured);
+      console.log(featured);
+    });
+}, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <input type="search" placeholder="Search a TV series" />
+        </form>
       </header>
+      {shows.map((show) => <Show key={show.id} {...show} />)}
     </div>
   );
 }
