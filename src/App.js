13f24d688736dbd02e6863a9b22ae5aba9d9e762
tusharkandpal <kandpal.tushar@gmail.com>
2021-01-04 18:34:55 +0530

    added theme

--- src/App.js
+++ src/App.js
@@ -1,59 +1,75 @@
 import "./App.css";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import Movie from "./components/Movie";
-import Loader from './assets/Loader.svg';
-import NoResult from './assets/NoResult.jpg';
+import Loader from "./assets/Loader.svg";
+import NoResult from "./assets/NoResult.jpg";
 
 const Featured_API = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}`;
 const Search_API = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&query=`;
 
 function App() {
-const [movies, setMovies] = useState([]);
-const [searchTerm, setSearchTerm] = useState('');
-const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
-const fetchResults = (API) => {
-  setIsLoading(true);
+  const fetchResults = (API) => {
+    setIsLoading(true);
 
-  fetch(API)
-  .then(res => res.json())
-  .then(data => {
-    setMovies(data.results);
-  })
-  .catch(err => console.log(err));
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      })
+      .catch((err) => console.log(err));
 
-  setIsLoading(false);
-}
+    setIsLoading(false);
+  };
 
-useEffect(() => {
-  fetchResults(Featured_API);
-}, []);
+  useEffect(() => {
+    fetchResults(Featured_API);
+  }, []);
 
-const handleChange = (e) => {
-  setSearchTerm(e.target.value);
-}
-const handleSubmit = (e) => {
-  e.preventDefault();
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  if (searchTerm) {
-    fetchResults(Search_API+searchTerm);
-    e.target.value='';
-  }
-  else fetchResults(Featured_API);
+    if (searchTerm) {
+      fetchResults(Search_API + searchTerm);
+      e.target.value = "";
+    } else fetchResults(Featured_API);
+  };
 
-}
+  const handleClick = () => {
+    const app = document.querySelector(".App");
+    app.classList.toggle("theme-dark");
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <form id='form' onSubmit={handleSubmit}>
-          <input id='search' type="search" placeholder="Search a movie" value={searchTerm} onChange={handleChange} />
+        <form id="form" onSubmit={handleSubmit}>
+          <input
+            id="search"
+            type="search"
+            placeholder="Search a movie"
+            value={searchTerm}
+            onChange={handleChange}
+          />
         </form>
+        <button id="theme" onClick={handleClick}></button>
       </header>
-      <div id='movies-container'>
-      {!isLoading && movies.length === 0 && <img src={NoResult} alt='no_result' />}
+      <div id="movies-container">
+        {!isLoading && movies.length === 0 && (
+          <img id="null_img" src={NoResult} alt="no_result" />
+        )}
 
-      {isLoading ? <img src={Loader} alt='loader'/> : movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+        {isLoading ? (
+          <img src={Loader} alt="loader" />
+        ) : (
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)
+        )}
       </div>
     </div>
   );
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,23 +4,21 @@ import React from "react";
 const ImageAPI = "https://image.tmdb.org/t/p/w1280/";
 
 function Movie({ title, poster_path, overview, vote_average }) {
-let voteStyle = {};
+  let voteStyle = {};
 
-    if (vote_average >= 8) {
-        voteStyle = {
-            color: "rgb(57, 191, 55)"
-        }  
-    }
-    else if (vote_average >= 6) {
-        voteStyle = {
-            color: "orange"
-        }
-    }
-    else if (vote_average >= 0) {
-        voteStyle = {
-            color: "red"
-        }
-    }
+  if (vote_average >= 8) {
+    voteStyle = {
+      color: "rgb(57, 191, 55)",
+    };
+  } else if (vote_average >= 6) {
+    voteStyle = {
+      color: "orange",
+    };
+  } else if (vote_average >= 0) {
+    voteStyle = {
+      color: "red",
+    };
+  }
 
   return (
     <div className="movie-box">
@@ -35,12 +33,14 @@ let voteStyle = {};
       />
       <div className="movie-info">
         <h4 className="movie-title">{title}</h4>
-        <span className="movie-rating" style={voteStyle}>{vote_average}</span>
+        <span className="movie-rating" style={voteStyle}>
+          {vote_average}
+        </span>
       </div>
-        <div className='overview'>
-            <h4>Overview:</h4>
-            <p>{overview}</p>
-        </div>
+      <div className="overview">
+        <h4 className="overview-title">Overview:</h4>
+        <p className="overview-info">{overview}</p>
+      </div>
     </div>
   );
 }
