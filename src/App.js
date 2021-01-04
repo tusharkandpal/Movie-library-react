8bbc2e9646de06dea502168b7bbde33fb4dc642f
tusharkandpal <kandpal.tushar@gmail.com>
2021-01-04 11:34:43 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
-import {useState, useEffect} from 'react';
 import "./App.css";
-import Show from "./components/Show";
+import {useState, useEffect} from 'react';
+import Movie from "./components/Movie";
+import Loader from './assets/Loader.svg';
+import NoResult from './assets/NoResult.jpg';
 
-const SearchAPI = "http://api.tvmaze.com/search/shows?q=";
-const FeaturedAPI = "http://api.tvmaze.com/shows?page=1";
+const Featured_API = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}`;
+const Search_API = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&query=`;
 
 function App() {
-const [shows, setShows] = useState([]);
+const [movies, setMovies] = useState([]);
 const [searchTerm, setSearchTerm] = useState('');
+const [isLoading, setIsLoading] = useState(true);
+
+const fetchResults = (API) => {
+  setIsLoading(true);
+
+  fetch(API)
+  .then(res => res.json())
+  .then(data => {
+    setMovies(data.results);
+  })
+  .catch(err => console.log(err));
+
+  setIsLoading(false);
+}
 
 useEffect(() => {
-  fetch(FeaturedAPI)
-    .then(res => res.json())
-    .then(data => {
-      const featured = data.filter((show) => show.id <= 350 )
-      setShows(featured);
-      console.log(featured);
-    });
-}, [])
+  fetchResults(Featured_API);
+}, []);
+
+const handleChange = (e) => {
+  setSearchTerm(e.target.value);
+}
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  if (searchTerm) {
+    fetchResults(Search_API+searchTerm);
+    e.target.value='';
+  }
+  else fetchResults(Featured_API);
+
+}
 
   return (
     <div className="App">
       <header className="App-header">
-        <form>
-          <input type="search" placeholder="Search a TV series" />
+        <form id='form' onSubmit={handleSubmit}>
+          <input id='search' type="search" placeholder="Search a movie" value={searchTerm} onChange={handleChange} />
         </form>
       </header>
-      {shows.map((show) => <Show key={show.id} {...show} />)}
+      <div id='movies-container'>
+      {!isLoading && movies.length === 0 && <img src={NoResult} alt='no_result' />}
+
+      {isLoading ? <img src={Loader} alt='loader'/> : movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
     </div>
   );
 }
---
+++ src/components/Movie.js
@@ -0,0 +1,48 @@
+import "./Movie.css";
+import React from "react";
+
+const ImageAPI = "https://image.tmdb.org/t/p/w1280/";
+
+function Movie({ title, poster_path, overview, vote_average }) {
+let voteStyle = {};
+
+    if (vote_average >= 8) {
+        voteStyle = {
+            color: "rgb(57, 191, 55)"
+        }  
+    }
+    else if (vote_average >= 6) {
+        voteStyle = {
+            color: "orange"
+        }
+    }
+    else if (vote_average >= 0) {
+        voteStyle = {
+            color: "red"
+        }
+    }
+
+  return (
+    <div className="movie-box">
+      <img
+        className="movie-img"
+        alt="img"
+        src={
+          poster_path
+            ? ImageAPI + poster_path
+            : "https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=925&q=80"
+        }
+      />
+      <div className="movie-info">
+        <h4 className="movie-title">{title}</h4>
+        <span className="movie-rating" style={voteStyle}>{vote_average}</span>
+      </div>
+        <div className='overview'>
+            <h4>Overview:</h4>
+            <p>{overview}</p>
+        </div>
+    </div>
+  );
+}
+
+export default Movie;
